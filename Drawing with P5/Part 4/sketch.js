307649c668f65a884261ccc2f86e532067b69e8f
Alec-Palmer-Hodges <111945515+Alec-Palmer-Hodges@users.noreply.github.com>
2023-01-24 21:52:09 -0600

    updated the star

--- Drawing with P5/Part 4/sketch.js
+++ Drawing with P5/Part 4/sketch.js
@@ -1,31 +1,26 @@
 function setup() {
-    createCanvas(200, 200);
-}
-
-function star(x, y, radius1, radius2, npoints) {
-    let angle = TWO_PI / npoints;
-    let halfAngle = angle / 2.0;
-
-    beginShape();
-    for (let a = 0; a < TWO_PI; a += angle) {
-        let sx = x + cos(a) * radius2;
-        let sy = y + sin(a) * radius2;
-        vertex(sx, sy);
-        sx = x + cos(a + halfAngle) * radius1;
-        sy = y + sin(a + halfAngle) * radius1;
-        vertex(sx, sy);
-    }
-    endShape(CLOSE);
+    createCanvas(400,400);
 }
 
 function draw() {
-    background(0, 0, 130);
-
-    stroke(255);
-    strokeWeight(3);
-    fill(0, 150, 0);
-    circle(100, 100 , 110);
-
-    fill(255, 0, 0);
-    star(width/ 2, height / 2, 25, 60, 5);
+  background(26,0,153);
+  
+  fill(0,153,0);
+  stroke(255);
+  strokeWeight(7);
+  ellipse(215,215,225,225);
+  
+  fill(255,0,0)
+  beginShape();
+  vertex(330,180)
+  vertex(250,180)
+  vertex(220,95)
+  vertex(180,180)
+  vertex(100,180)
+  vertex(165,235)
+  vertex(140,305)
+  vertex(215,265)
+  vertex(290,305)
+  vertex(265,235)
+  endShape(CLOSE);
 }
