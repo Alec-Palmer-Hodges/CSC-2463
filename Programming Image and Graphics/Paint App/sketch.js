ea8c57d6ce27fca0fc88ad18fbb4b14e5a2d58c9
Alec-Palmer-Hodges <111945515+Alec-Palmer-Hodges@users.noreply.github.com>
2023-01-24 20:02:59 -0600

    modified the html files for assignment 1 - 4 to display which assignment numbers they are.

--- Programming Image and Graphics/Paint App/sketch.js
+++ Programming Image and Graphics/Paint App/sketch.js
@@ -1,46 +1,78 @@
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(window.innerWidth, window.innerHeight);
     colorMode(RGB, 255, 255, 255, 1);
-    background(255);
-    strokeWeight(5);
-    stroke(0);
+    background(235);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     noStroke();
-
+    // Red
     fill(255, 0, 0);
-    square(0, 10, 25);
-
+    square(5, 10, 25);
+    // Orange
+    fill(255, 165, 0);
+    square(5, 40, 25);
+    // Yellow
+    fill(255, 255, 0);
+    square(5, 70, 25);
+    // Green
     fill(0, 255, 0);
-    square(0, 40, 25);
-
+    square(5, 100, 25);
+    // Cyan
+    fill(0, 255, 255);
+    square(5, 130, 25);
+    // Blue
+    fill(0, 0, 128);
+    square(5, 160, 25);
+    // Magenta
     fill(255, 0, 255);
-    square(0, 70, 25);
-
-    fill(255, 255, 0);
-    square(0, 100, 25);
-
-    fill(255, 0, 0);
-    square(0, 130, 25);
-
-    fill(0, 255, 0);
-    square(0, 160, 25);
-
-    fill(255, 0, 255);
-    square(0, 190, 25);
-
-    fill(255, 255, 0);
-    square(0, 220, 25);
-
-    fill(255, 0, 255);
-    square(0, 250, 25);
-
-    fill(255, 255, 0);
-    square(0, 280, 25);
+    square(5, 190, 25);
+    // Brown
+    fill(139, 69, 19);
+    square(5, 220, 25);
+    // White
+    fill(255, 255, 255);
+    square(5, 250, 25);
+    // Black
+    fill(0, 0, 0);
+    square(5, 280, 25);
 }
 
-function touchMoved() {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+function mouseIsPressed() {
+    if (collide(5, 10)) {
+        selected = "red";
+    } else if (collide (5, 40)) {
+        selected = "orange";
+    }  else if (collide (5, 70)) {
+        selected = "yellow";
+    }  else if (collide (5, 100)) {
+        selected = "green";
+    }  else if (collide (5, 130)) {
+        selected = "cyan";
+    }  else if (collide (5, 160)) {
+        selected = "blue";
+    }  else if (collide (5, 190)) {
+        selected = "magenta";
+    }  else if (collide (5, 220)) {
+        selected = "brown";
+    }  else if (collide (5, 250)) {
+        selected = "white";
+    }  else if (collide (5, 280)) {
+        selected = "black";
+    } 
+}
+
+function collide ( x, y) {
+    //2d
+    if (mouseX >= x &&         // right of the left edge AND
+        mouseX <= x + 20 &&    // left of the right edge AND
+        mouseY >= y &&         // below the top AND
+        mouseY <= y + 20) {    // above the bottom
+            return true;
+    }
     return false;
-}
+    };
--- Programming Image and Graphics/Sprite Animation/sketch.js
+++ Programming Image and Graphics/Sprite Animation/sketch.js
@@ -1,34 +1,26 @@
-let spritesheet;
-let spritedata;
+let spriteSheet;
+let spriteData;
 
 let animation = [];
 
-let sprite = [];
+let sprite= [];
 
 function preload() {
-    spritedata = loadJSON('Sprite Animation\sprite\sprite.json');
-    spritesheet = loadImage('Sprite Animation\sprite\spelunky-guy.png');
+   // spriteData = loadJSON('sprite.json');
+    spriteSheet = loadImage('Green.png');
 }
 
 function setup() {
-    createCanvas(640, 480);
-    let frames = spritedata.frames;
-    for (let i = 0; i < frames.length; i++) {
-        let pos = frames[i].position;
-        let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
-        animation.push(img);
-    }
+    createCanvas(windowWidth, windowHeight);
 
-    for (let i = 0; i < 2; i++) {
-        sprites[i] = new Sprite(animation, 0, i * 75, random(0.1, 0.4));
+    let frames = spriteData.frames;
+    for (let i = 0; i < 9; i++) {
+        let img = spriteSheet.get(i * 80, 0, 80, 80);
+        animation.push(img);
     }
 }
 
 function draw() {
-    background(0);
-
-    for (let sprite of sprites) {
-        sprite.show();
-        sprite.animate();
-    }
+    background(255);
+    
 }
