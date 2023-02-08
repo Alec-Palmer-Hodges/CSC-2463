d80eab6ad15dc7e769118d16dd248daf4d08cdea
Alec-Palmer-Hodges <111945515+Alec-Palmer-Hodges@users.noreply.github.com>
2023-02-07 21:48:08 -0600

    Formatted the documents a little better

--- Sprite Animation/sketch.js
+++ Sprite Animation/sketch.js
@@ -1,36 +1,39 @@
-let spritesheet=[];
+let spritesheet = [];
 let animationSet = [];
 let yLocation = 0;
 let characters = [];
 
 function preload() {
-  spritesheet = [loadImage('../Assets/Green.png'),
-                 loadImage('../Assets/Spelunky Guy.png')];
+  spritesheet = [
+    loadImage("../Assets/Green.png"),
+    loadImage("../Assets/Spelunky Guy.png"),
+  ];
 }
 
-
 function setup() {
-  createCanvas(windowWidth - 20,windowHeight - 20);
-  for(sheet of spritesheet){
+  createCanvas(windowWidth - 20, windowHeight - 20);
+  for (sheet of spritesheet) {
     for (let i = 0; i < 9; i++) {
-      let img = sheet.get(80*i, 0, 80, 80);
+      let img = sheet.get(80 * i, 0, 80, 80);
       animationSet.push(img);
-    } 
-    characters.push(new Sprite(animationSet,random()*(width-80),yLocation+200));
-    yLocation+= height/4;
+    }
+    characters.push(
+      new Sprite(animationSet, random() * (width - 80), yLocation + 200)
+    );
+    yLocation += height / 4;
     animationSet = [];
   }
 }
 
 function draw() {
   background(255);
-  for(let sprite of characters){
+  for (let sprite of characters) {
     sprite.show();
-    if(keyIsDown(RIGHT_ARROW)){
+    if (keyIsDown(RIGHT_ARROW)) {
       sprite.walkRight();
-    }else if(keyIsDown(LEFT_ARROW)){
+    } else if (keyIsDown(LEFT_ARROW)) {
       sprite.walkLeft();
-    } else{
+    } else {
       sprite.stand();
     }
   }
